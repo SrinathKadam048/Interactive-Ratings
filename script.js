ce84bb865f313ee06c56a2d0377f23e917047d51
Srinath Kadam <srinathkadam@Srinaths-MacBook-Air.local>
2024-04-01 20:53:02 +0530

    final changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const ratings = document.querySelectorAll(".rating")
 const submit = document.querySelector(".submit-button");
 const thanks = document.querySelector(".thanks-content");
+const ratingContent = document.querySelector('.rating-content')
 
 var clickedRating = 0;
 
@@ -17,6 +18,9 @@ ratings.forEach(rating => {
 })
 
 submit.addEventListener("click", () => {
-    thanks.style.display = "block"
+    ratingContent.style.display = "none";
+    thanks.style.display = "block";
+    choice = document.querySelector('.rating-choice');
+    choice.innerText = `You chose a rating of ${clickedRating} out of 5`
 })
 
